890ca1eb10cf566eb3340228541ec0c561579ba9
Maxim <maxrap7878@mail.ru>
2018-03-21 16:35:17 +0200

    add hello world

--- 1000things/App.js
+++ 1000things/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
+
+
+
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello world !</Text>
+          <Button style={styles.button}
+
+              title="Go to Volodarka"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+
+          />
+
+
       </View>
     );
   }
@@ -20,4 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    button: {
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#841584',
+    },
 });

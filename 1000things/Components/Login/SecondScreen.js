5a89c0566c302f3297d4dea9cba04797e63f2471
Maxim <maxrap7878@mail.ru>
2018-04-10 22:17:41 +0300

    second screen style sheet v1

--- 1000things/Components/Login/SecondScreen.js
+++ 1000things/Components/Login/SecondScreen.js
@@ -1,19 +1,50 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View, StyleSheet, Image, Button, KeyboardAvoidingView} from 'react-native';
+import {AppRegistry, Text, View, StyleSheet, Image} from 'react-native';
+import { Constants } from 'expo';
+import Button  from "apsl-react-native-button";
 
 
 export default class Second extends Component{
   render(){
     return(
 
-        <View style={styles.logoContainer}>
-        <Text>Hello World !</Text>
-          <Button style ={styles.buttonText}
+        <View style={styles.container}>
+          <Text style={styles.titileText}>Виберіть пріорітетність діяльності</Text>
 
-            title="Go Login"
-            color="#2ecc71"
+          <Button
+            style={styles.buttonSt}
+
+
+
+
             accessibilityLabel="Learn more about this purple button"
-            />
+
+
+
+            >Спорт</Button>
+
+            <Button
+              style={styles.buttonSt}
+
+
+
+              accessibilityLabel="Learn more about this purple button"
+              >Творчість</Button>
+              <Button
+                style={styles.buttonSt}
+
+
+
+                accessibilityLabel="Learn more about this purple button"
+                >Соціальне</Button>
+                <Button
+                  style={styles.buttonSt}
+
+
+
+  color= "white"
+                  accessibilityLabel="Learn more about this purple button"
+                  >123</Button>
 
         </View>
 
@@ -21,18 +52,36 @@ export default class Second extends Component{
   }
 }
 const styles  = StyleSheet.create({
+
   container:{
+    backgroundColor: '#ecf0f1',
     flex: 1,
-    backgroundColor: '#ecf0f1'
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+
   },
   logoContainer:{
     alignItems: 'center',
     flexGrow: 1,
     justifyContent: 'center'
   },
-  logo:{
-    width: 100,
-    height: 100
+  buttonSt:{
+    backgroundColor:"#bdc3c7",
+    marginBottom:20,
+    height: 60,
+    width: 280,
+    borderRadius: 0,
+    borderColor: "#bdc3c7",
+    position: "relative",
+    marginLeft: 20,
+
+  },
+  titileText:{
+    marginBottom: 20,
+    marginTop: 20,
+
   }
+
 
 });

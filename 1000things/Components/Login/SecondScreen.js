468445a9394535eed24eb840600880d4656d080c
Maxim <maxrap7878@mail.ru>
2018-04-13 23:08:41 +0300

    add active button

--- 1000things/Components/Login/SecondScreen.js
+++ 1000things/Components/Login/SecondScreen.js
@@ -27,13 +27,12 @@ export default class Second extends Component{
     this.ColorChange3 = this.ColorChange3.bind(this);
     this.state =
      {isClicked: 1,
-      isClicked1: false,
-      isClicked2: false,
-      isClicked3: false,
+      isClicked1: 1,
+      isClicked2: 1,
+      isClicked3: 1,
      };
 }
      ColorChange(){
-
 if(this.state.isClicked === 1){
       this.setState({
          isClicked: 2
@@ -47,6 +46,10 @@ if(this.state.isClicked === 1){
       this.setState({
        isClicked: 4
          });
+    }else if(this.state.isClicked === 4){
+      this.setState({
+       isClicked: 5
+         });
     }else{
       this.setState({
        isClicked: this.state.isClicked
@@ -54,59 +57,81 @@ if(this.state.isClicked === 1){
     };
     };
     ColorChange1(){
-     this.setState({
-       isClicked1: !this.state.isClicked1
-     });
+      if(this.state.isClicked1 === 1){
+            this.setState({
+               isClicked1: 2
+            });
+      }
+          else if(this.state.isClicked1 === 2){
+            this.setState({
+             isClicked1: 3
+               });
+          }else if(this.state.isClicked1 === 3){
+            this.setState({
+             isClicked1: 4
+               });
+          }else if(this.state.isClicked1 === 4){
+            this.setState({
+             isClicked1: 5
+               });
+          }else{
+            this.setState({
+             isClicked1: this.state.isClicked1
+               });
+          };
    };
    ColorChange2(){
-    this.setState({
-      isClicked2: !this.state.isClicked2
-    });
+     if(this.state.isClicked2 === 1){
+           this.setState({
+              isClicked2: 2
+           });
+     }
+         else if(this.state.isClicked2 === 2){
+           this.setState({
+            isClicked2: 3
+              });
+         }else if(this.state.isClicked2 === 3){
+           this.setState({
+            isClicked2: 4
+              });
+         }else if(this.state.isClicked2 === 4){
+           this.setState({
+            isClicked2: 5
+              });
+         }else{
+           this.setState({
+            isClicked2: this.state.isClicked1
+              });
+         };
+
   };
   ColorChange3(){
-   this.setState({
-     isClicked3: !this.state.isClicked3
-   });
+    if(this.state.isClicked3 === 1){
+          this.setState({
+             isClicked3: 2
+          });
+    }
+        else if(this.state.isClicked3 === 2){
+          this.setState({
+           isClicked3: 3
+             });
+        }else if(this.state.isClicked3 === 3){
+          this.setState({
+           isClicked3: 4
+             });
+        }else if(this.state.isClicked3 === 4){
+          this.setState({
+           isClicked3: 5
+             });
+        }else{
+          this.setState({
+           isClicked3: this.state.isClicked3
+             });
+        };
  }
 
 ColorShade(isClicked){
    if(isClicked === 1){
-     return{
-       backgroundColor:"#8e44ad",
-       marginBottom:20,
-         height: 60,
-         width: 280,
-         borderRadius: 0,
-         borderColor: "#27ae60",
-         position: "relative",
-         marginLeft: 20,
-
-     }
-   }else if (isClicked === 2 ){
-     return{
-       backgroundColor:"#2980b9",
-       marginBottom:20,
-         height: 60,
-         width: 280,
-         borderRadius: 0,
-         borderColor: "#27ae60",
-         position: "relative",
-         marginLeft: 20,
-
-     }
-   }else if(isClicked === 3){
-     return{
-       backgroundColor:"#e74c3c",
-        marginBottom:20,
-         height: 60,
-         width: 280,
-         borderRadius: 0,
-         borderColor: "#2c3e50",
-         position: "relative",
-         marginLeft: 20
-
-     }
-   }else {
      return{
        backgroundColor:"#bdc3c7",
        marginBottom:20,
@@ -115,10 +140,67 @@ ColorShade(isClicked){
          borderRadius: 0,
          borderColor: "#bdc3c7",
          position: "relative",
+         marginLeft: 20,
+
+     }
+   }else if (isClicked === 2 ){
+     return{
+       backgroundColor:"rgba(39, 174, 96,0.3)",
+       marginBottom:20,
+         height: 60,
+         width: 280,
+         borderRadius: 0,
+         borderColor: "rgba(39, 174, 96,0.3)",
+         position: "relative",
+         marginLeft: 20,
+
+     }
+   }else if(isClicked === 3){
+     return{
+       backgroundColor:"rgba(39, 174, 96,0.5)",
+        marginBottom:20,
+         height: 60,
+         width: 280,
+         borderRadius: 0,
+         borderColor: "rgba(39, 174, 96,0.5)",
+         position: "relative",
+         marginLeft: 20
+
+     }
+   }else if(isClicked === 4){
+     return{
+       backgroundColor:"rgba(39, 174, 96,0.8)",
+        marginBottom:20,
+         height: 60,
+         width: 280,
+         borderRadius: 0,
+         borderColor: "rgba(39, 174, 96,0.8)",
+         position: "relative",
+         marginLeft: 20
+
+     }
+   }else {
+     return{
+       backgroundColor:"rgba(39, 174, 96,1.0)",
+       marginBottom:20,
+         height: 60,
+         width: 280,
+         borderRadius: 0,
+         borderColor: "rgba(39, 174, 96,1.0)",
+         position: "relative",
          marginLeft: 20
 
      }
    }
+
+ }
+ Ready(first,second,third,fouth){
+   if(first!== 1 && second!==1 && third!==1 && fouth!==1){
+     return(false)
+   }else{
+     return(true)
+
+        }
 
  }
 
@@ -141,67 +223,30 @@ ColorShade(isClicked){
           <ButtonR
               textStyle={{color:"white"}}
               onPress={this.ColorChange1}
-              style={this.state.isClicked1 ?{backgroundColor:"#27ae60",  marginBottom:20,
-                height: 60,
-                width: 280,
-                borderRadius: 0,
-                borderColor: "#27ae60",
-                position: "relative",
-                marginLeft: 20,}
-                :{backgroundColor:"#bdc3c7",  marginBottom:20,
-                height: 60,
-                width: 280,
-                borderRadius: 0,
-                borderColor: "#bdc3c7",
-                position: "relative",
-                marginLeft: 20, }}
+              style={this.ColorShade(this.state.isClicked1)}
 
               accessibilityLabel="Learn more about this purple button"
               >Соціальне</ButtonR>
             <ButtonR
                 textStyle={{color:"white"}}
                 onPress={this.ColorChange2}
-                style={this.state.isClicked2 ?{backgroundColor:"#27ae60",  marginBottom:20,
-                  height: 60,
-                  width: 280,
-                  borderRadius: 0,
-                  borderColor: "#27ae60",
-                  position: "relative",
-                  marginLeft: 20,}
-                  :{backgroundColor:"#bdc3c7",  marginBottom:20,
-                  height: 60,
-                  width: 280,
-                  borderRadius: 0,
-                  borderColor: "#bdc3c7",
-                  position: "relative",
-                  marginLeft: 20, }}
+                style={this.ColorShade(this.state.isClicked2)}
 
                 accessibilityLabel="Learn more about this purple button"
                 >Творічість</ButtonR>
               <ButtonR
                   textStyle={{color:"white"}}
                   onPress={this.ColorChange3}
-                  style={this.state.isClicked3 ?{backgroundColor:"#27ae60",  marginBottom:20,
-                    height: 60,
-                    width: 280,
-                    borderRadius: 0,
-                    borderColor: "#27ae60",
-                    position: "relative",
-                    marginLeft: 20,}
-                    :{backgroundColor:"#bdc3c7",  marginBottom:20,
-                    height: 60,
-                    width: 280,
-                    borderRadius: 0,
-                    borderColor: "#bdc3c7",
-                    position: "relative",
-                    marginLeft: 20, }}
+                  style={this.ColorShade(this.state.isClicked3)}
 
                   accessibilityLabel="Learn more about this purple button"
                   >Відпочинок</ButtonR>
                   <Button
                     title="Go to Profile"
-                    color="#2ecc71"
+                    color = "rgba(46, 204, 113,1.0)"
+
                     accessibilityLabel="Learn more about this purple button"
+                    disabled = {this.Ready(this.state.isClicked,this.state.isClicked1,this.state.isClicked2,this.state.isClicked3)}
 
                     onPress={
                        ()=> navigate("Third",{})
